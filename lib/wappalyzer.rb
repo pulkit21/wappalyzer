16b4f6b577c732ce05d7f2a05abb26aceff28e14
Philippe Demontigny <phdemontigny@gmail.com>
2016-11-14 16:39:13 -0500

    Fixed UTF8 Errors

--- lib/wappalyzer.rb
+++ lib/wappalyzer.rb
@@ -5,6 +5,7 @@ require "wappalyzer/version"
 require 'net/http'
 require 'mini_racer'
 require 'json'
+require 'zlib'
 
 Encoding.default_external = Encoding::UTF_8
 
@@ -17,15 +18,24 @@ module Wappalyzer
       @categories, @apps = @json['categories'], @json['apps']
     end
 
+    def utf8_encoding(str)
+      str.encode('UTF-8', :invalid => :replace, :undef => :replace)
+    end
+
     def analyze(url)
       uri, body, headers = URI(url), nil, {}
       Net::HTTP.start(uri.host, uri.port,
                       :use_ssl => uri.scheme == 'https',
                       :verify_mode => OpenSSL::SSL::VERIFY_NONE,
                       :open_timeout => 5) do |http|
-        resp = http.get(uri.request_uri, "Accept-Encoding" => "none")
-        resp.each_header{|k,v| headers[k.downcase] = v}
-        body = resp.body.encode('UTF-8', :invalid => :replace, :undef => :replace)
+        begin
+          resp = http.get(uri.request_uri)
+        rescue Zlib::DataError
+          resp = http.get(uri.request_uri, "Accept-Encoding" => "none")
+        end
+
+        resp.each_header { |k,v| headers[k.downcase] = utf8_encoding(v) }
+        body = utf8_encoding(resp.body)
       end
 
       cxt = MiniRacer::Context.new
